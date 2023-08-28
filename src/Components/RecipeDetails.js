e3ebbe43bd4d5b57dd4985260fa987ed46e29b42
AliyuAliyu1 <109520063+AliyuAliyu1@users.noreply.github.com>
2023-08-28 17:18:56 +0100

    updates

--- src/Components/RecipeDetails.js
+++ src/Components/RecipeDetails.js
@@ -2,6 +2,73 @@ const recipeDetails = {
     name: 'Pounded Yam',
     image: 'https://thumbs.dreamstime.com/z/egusi-soup-pounded-yam-nigerian-cuisine-isolated-white-background-62743456.jpg?w=768',
 
+ /*   
+    meal_portion : [
+{
+"pounded yam":{
+    pounde_yam_ingridents:{
+    
+      'boiled yam':[],
+       'cup of water':[]
+
+              },
+        Image_of_pounded_yam:[]
+
+     },
+
+"Egusi soup":{
+    egusi_ingredients:{
+        '1 cup blended onions':[],
+        '4 cups egusi':[],
+        'half to 1 cup palm oil':[],
+        '2 teaspoons fresh Une(locust beans)':[],
+        'Ground crayfish':[],
+        'Salt':[],
+        '7 to 8 cups stock':[],
+        'Cooked Meat & fish':[],
+        '2 cups cut pumpkin leaves':[],
+        '1 cup waterleaf (cut)':[],
+        '3 tablespoons bitter leaf (washed)':[],
+        
+
+    
+    }
+    ,
+Image_of_egusi_soup:[]
+    
+    },
+    "boiled yam":{
+        boiled_yam_ingredients:{
+            'yam':[],
+            'water':[],
+            'salt':[],   
+        
+        },
+        Image_of_boiled_yam:[]
+        
+        },
+        
+  
+
+
+} 
+   
+       ],
+
+instructions: [{
+
+    'Rinse the rice thoroughly and set aside.':[],
+    'Chop the onions and bell peppers.':[],
+    'In a pot, heat the vegetable oil and sauté the onions and bell peppers.':[],
+    'Add the tomato paste and cook for a few minutes.':[],
+    notes: 'pounded yam is a popular Nigerian dish often served at celebrations.',
+    // Add more instructions as needed
+}
+
+      ],       
+    
+*/
+
     meal_portion : [
      'pounded yam',
      'Egusi',
@@ -10,6 +77,8 @@ const recipeDetails = {
     ],
 
     pounded_yam_ingredients: [
+
+        
       'boiled yam',
       'cup of water',
      
